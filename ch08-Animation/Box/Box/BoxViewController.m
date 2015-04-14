773da39fd47f741f3923a7c893fa674fdf9bc1a6
chksong <chksong@qq.com>
2015-04-14 16:58:09 +0800

    //    You can find the full details here. Note that Apple uses the reversed multiplication order for projection (relative to the given link) so all matrix multiplications are reversed and all matrices are transposed. // //        A short description of the meaning: // //        m34 = 1/z distance to projection plane //        + for the z axis is towards the viewer resulting in a "looking in the mirror" feel when using - //            projection center is (0,0,0) plus any translations you set up
    //    你可以在这里找到的全部细节。需要注意的是苹果使用的逆乘法顺序投影（相对于给定的链路），所以所有的矩阵乘法被颠倒并且所有矩阵转置。
    //    含义的简要说明：
    //    M34=1/ Z距离投影面
    //    + Z轴是走向产生时，使用的是“照镜子”感觉观众 -
    //    投影中心为（0,0,0）加任何翻译设置了

--- ch08-Animation/Box/Box/BoxViewController.m
+++ ch08-Animation/Box/Box/BoxViewController.m
@@ -44,8 +44,24 @@ const CGFloat kPanScale = 1./100.;
 }
 
 static CATransform3D MakePerspetiveTransform() {
+
+//    You can find the full details here. Note that Apple uses the reversed multiplication order for projection (relative to the given link) so all matrix multiplications are reversed and all matrices are transposed.
+//        
+//        A short description of the meaning:
+//        
+//        m34 = 1/z distance to projection plane
+//        + for the z axis is towards the viewer resulting in a "looking in the mirror" feel when using -
+//            projection center is (0,0,0) plus any translations you set up
+    
+    
+//    你可以在这里找到的全部细节。需要注意的是苹果使用的逆乘法顺序投影（相对于给定的链路），所以所有的矩阵乘法被颠倒并且所有矩阵转置。
+//    含义的简要说明：
+//    M34=1/ Z距离投影面
+//    + Z轴是走向产生时，使用的是“照镜子”感觉观众 -
+//    投影中心为（0,0,0）加任何翻译设置了
+    
   CATransform3D perspective = CATransform3DIdentity;
-  perspective.m34 = -1./2000.;
+  perspective.m34 = -1./100.;
   return perspective;
 }
 
@@ -93,14 +109,19 @@ static CATransform3D MakePerspetiveTransform() {
 }
 
 - (void)pan:(UIPanGestureRecognizer *)recognizer {
+ //返回距离起始点。。。。
   CGPoint translation = [recognizer translationInView:self.view];
+
   CATransform3D transform = MakePerspetiveTransform();
   transform = CATransform3DRotate(transform, 
                                   kPanScale * translation.x,
                                   0, 1, 0);
+    
+ NSLog(@"[pan] .... xangle=%f   yangle=%f" ,   translation.x ,   translation.y);
   transform = CATransform3DRotate(transform,
                                   -kPanScale * translation.y,
                                   1, 0, 0);
+    
   self.view.layer.sublayerTransform = transform;
 }
 
