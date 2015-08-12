1c4e051969dc8cbcdb665f7ed59f36f1c4640b2a
chksong <chksong@qq.com>
2015-08-12 11:07:04 +0800

    t添加注释

--- ch02-UI Paradigms/BlurDemo/BlurDemo/SCTViewController.m
+++ ch02-UI Paradigms/BlurDemo/BlurDemo/SCTViewController.m
@@ -25,25 +25,29 @@
 
 -(IBAction)buttonAction:(id)sender {
   
+  //创建图层。
   self.layer = [CALayer layer];
   self.layer.frame = CGRectMake(80, 100, 160, 160);
   [self.view.layer addSublayer:self.layer];
 
+  // 截屏
   float scale = [UIScreen mainScreen].scale;
   UIGraphicsBeginImageContextWithOptions(self.view.frame.size, YES, scale);
   [self.view drawViewHierarchyInRect:self.view.frame afterScreenUpdates:NO];
   __block UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
   UIGraphicsEndImageContext();
 
+  //剪切截图。
   CGImageRef imageRef = CGImageCreateWithImageInRect(image.CGImage,
                                                      CGRectMake(self.layer.frame.origin.x * scale,
                                                                 self.layer.frame.origin.y * scale,
+                      
                                                                 self.layer.frame.size.width * scale,
                                                                 self.layer.frame.size.height * scale));
   image = [UIImage imageWithCGImage:imageRef];
-  image = [image applyBlurWithRadius:50.0f
+  image = [image applyBlurWithRadius:1.0f
                            tintColor:[UIColor colorWithRed:0 green:1 blue:0 alpha:0.1]
-               saturationDeltaFactor:2.0f
+               saturationDeltaFactor:1.0f
                            maskImage:nil];
 
   self.layer.contents = (__bridge id)(image.CGImage);
